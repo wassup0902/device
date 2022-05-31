f55a1b2095dbc1faace4862e8955e3fca391b42c
Your Name <radak91@mail.ru>
2022-06-01 00:32:03 +0800

    third markup index
    finish markup header, made first popup catalog menu, add js file for this.

---
+++ js/header-catalog-menu-show.js
@@ -0,0 +1,7 @@
+var modalButton1 = document.querySelector(".main-header-catalog");
+var modal1 = document.querySelector(".header-catalog-menu");
+
+modalButton1.addEventListener("click", function (evt) {
+	evt.preventDefault();
+	modal1.classList.toggle("header-catalog-menu-show");
+});
